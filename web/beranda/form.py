05e4e3b885e8a0d8f7d8c9555f6bc06332b05087
golDroger999 <goldr1711@gmail.com>
2022-08-15 22:33:02 +0800

    ngerjain frontend dubois form

--- web/beranda/form.py
+++ web/beranda/form.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import (StringField, IntegerField, FloatField, SubmitField, RadioField)
+from wtforms import (StringField, IntegerField, FloatField, SubmitField, RadioField, SelectField)
 from wtforms.validators import (DataRequired, Email)
 
 
@@ -8,11 +8,11 @@ from wtforms.validators import (DataRequired, Email)
 class FormDubois(FlaskForm):
     nama = StringField('Nama', validators=[DataRequired(message='data harus diisi')])
     umur = IntegerField('Umur', validators=[DataRequired(message='data harus diisi')])
-    bb = FloatField('Berat Badan', validators=[DataRequired(message='data harus diisi')])
-    tb = FloatField('Tinggi Badan', validators=[DataRequired(message='data harus diisi')])
+    bb = IntegerField('Berat Badan', validators=[DataRequired(message='data harus diisi')])
+    tb = IntegerField('Tinggi Badan', validators=[DataRequired(message='data harus diisi')])
     aktivitas = FloatField('Aktivitas', validators=[DataRequired(message='data harus diisi')])
     waktu_tidur = IntegerField('Waktu Tidur', validators=[DataRequired(message='data harus diisi')])
-    gender = RadioField('Gender', choices=['PRIA', 'WANITA'])
+    gender = SelectField('Gender', choices=['pria', 'wanita'])
     hitung = SubmitField('Hitung', validators=[DataRequired(message='data harus diisi')])
     laporan = SubmitField('Laporan', validators=[DataRequired(message='data harus diisi')])
     
@@ -24,7 +24,7 @@ class FormHarrisBenneedict(FlaskForm):
     tb = FloatField('Tinggi Badan', validators=[DataRequired(message='data harus diisi')])
     aktivitas = FloatField('Aktivitas', validators=[DataRequired(message='data harus diisi')])
     stress = FloatField('Faktor Stress', validators=[DataRequired(message='data harus diisi')])
-    gender = RadioField('Gender', choices=['PRIA', 'WANITA'])
+    gender = SelectField('Gender', choices=['PRIA', 'WANITA'])
     hitung = SubmitField('Hitung', validators=[DataRequired(message='data harus diisi')])
     laporan = SubmitField('Laporan', validators=[DataRequired(message='data harus diisi')])
     
@@ -36,7 +36,7 @@ class FormMifflin(FlaskForm):
     tb = FloatField('Tinggi Badan', validators=[DataRequired(message='data harus diisi')])
     aktivitas = FloatField('Aktivitas', validators=[DataRequired(message='data harus diisi')])
     stress = FloatField('Faktor Stress', validators=[DataRequired(message='data harus diisi')])
-    gender = RadioField('Gender', choices=['PRIA', 'WANITA'])
+    gender = SelectField('Gender', choices=['PRIA', 'WANITA'])
     hitung = SubmitField('Hitung', validators=[DataRequired(message='data harus diisi')])
     laporan = SubmitField('Laporan', validators=[DataRequired(message='data harus diisi')])
     
--- web/beranda/routes.py
+++ web/beranda/routes.py
@@ -1,5 +1,6 @@
 from flask import (render_template, redirect, Request, Blueprint, url_for)
 from web.beranda.form import (FormDubois, FormHarrisBenneedict, FormMifflin)
+ 
 
 
 beranda = Blueprint('beranda', __name__)
@@ -7,32 +8,32 @@ beranda = Blueprint('beranda', __name__)
 @beranda.route('/beranda')
 @beranda.route('/')
 def beranda_page():
-    return render_template('beranda.html')
+    return render_template('beranda.html', tittle='NUTRIECY')
 
 
 @beranda.route('/alat-analisis')
 def analisis_page():
-    return render_template('alat_analisis.html')
+    return render_template('alat_analisis.html', tittle='ANALISIS GIZI')
 
 
 @beranda.route('/dashboard-page')
 def dashboard_page():
-    return render_template('dashboard.html')
+    return render_template('dashboard.html', tittle='DASHBOARD')
 
 
 @beranda.route('/dubois')
 def dubois_page():
     form = FormDubois() 
-    return render_template('dubois.html', form=form )
+    return render_template('dubois.html', form=form, tittle='DUBOIS')
 
 
 @beranda.route('/harris-bennedict')
 def harris_bennedict_page():
     form = FormHarrisBenneedict() 
-    return render_template('harris.html', form=form)
+    return render_template('harris.html', form=form, tittle='HARRIS BENNEDICT')
 
 
 @beranda.route('/mifflin')
 def mifflin_page():
     form = FormMifflin()
-    return render_template('mifflin.html', form=form)
+    return render_template('mifflin.html', form=form, tittle='MIFFLIN')
