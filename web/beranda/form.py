21fcdf5f88edc809f6d7aaea0ecdf3d2622c4d5f
golDroger999 <goldr1711@gmail.com>
2022-08-19 22:57:42 +0800

    selesaikan harris dan mifflin

--- web/beranda/form.py
+++ web/beranda/form.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import (StringField, IntegerField, FloatField, SubmitField, RadioField, SelectField)
+from wtforms import (StringField, IntegerField, FloatField, SubmitField, RadioField, SelectField, DecimalField)
 from wtforms.validators import (DataRequired, Email)
 
 
@@ -8,36 +8,36 @@ from wtforms.validators import (DataRequired, Email)
 class FormDubois(FlaskForm):
     nama = StringField('Nama', validators=[DataRequired(message='data harus diisi')])
     umur = IntegerField('Umur', validators=[DataRequired(message='data harus diisi')])
-    bb = IntegerField('Berat Badan', validators=[DataRequired(message='data harus diisi')])
-    tb = IntegerField('Tinggi Badan', validators=[DataRequired(message='data harus diisi')])
+    bb = IntegerField('Berat Badan (kg)', validators=[DataRequired(message='data harus diisi')])
+    tb = IntegerField('Tinggi Badan (cm)', validators=[DataRequired(message='data harus diisi')])
     aktivitas = FloatField('Aktivitas', validators=[DataRequired(message='data harus diisi')])
     waktu_tidur = IntegerField('Waktu Tidur', validators=[DataRequired(message='data harus diisi')])
     gender = SelectField('Gender', choices=['pria', 'wanita'])
-    hitung = SubmitField('Hitung', validators=[DataRequired(message='data harus diisi')])
-    laporan = SubmitField('Laporan', validators=[DataRequired(message='data harus diisi')])
-    
+    hitung = SubmitField('Hitung')
+    laporan = SubmitField('Laporan')
+    save = SubmitField('Save Data')
 
 class FormHarrisBenneedict(FlaskForm):
     nama = StringField('Nama', validators=[DataRequired(message='data harus diisi')])
     umur = IntegerField('Umur', validators=[DataRequired(message='data harus diisi')])
-    bb = FloatField('Berat Badan', validators=[DataRequired(message='data harus diisi')])
-    tb = FloatField('Tinggi Badan', validators=[DataRequired(message='data harus diisi')])
-    aktivitas = FloatField('Aktivitas', validators=[DataRequired(message='data harus diisi')])
-    stress = FloatField('Faktor Stress', validators=[DataRequired(message='data harus diisi')])
-    gender = SelectField('Gender', choices=['PRIA', 'WANITA'])
-    hitung = SubmitField('Hitung', validators=[DataRequired(message='data harus diisi')])
-    laporan = SubmitField('Laporan', validators=[DataRequired(message='data harus diisi')])
+    bb = IntegerField('Berat Badan (kg)', validators=[DataRequired(message='data harus diisi')])
+    tb = IntegerField('Tinggi Badan (cm)', validators=[DataRequired(message='data harus diisi')])
+    aktivitas = IntegerField('Aktivitas', validators=[DataRequired(message='data harus diisi')])
+    stress = IntegerField('Faktor Stress', validators=[DataRequired(message='data harus diisi')])
+    gender = SelectField('Gender', choices=['pria', 'wanita'])
+    hitung = SubmitField('Hitung')
+    laporan = SubmitField('Laporan')
+    save = SubmitField('Save Data')
     
 
 class FormMifflin(FlaskForm):
     nama = StringField('Nama', validators=[DataRequired(message='data harus diisi')])
     umur = IntegerField('Umur', validators=[DataRequired(message='data harus diisi')])
-    bb = FloatField('Berat Badan', validators=[DataRequired(message='data harus diisi')])
-    tb = FloatField('Tinggi Badan', validators=[DataRequired(message='data harus diisi')])
+    bb = IntegerField('Berat Badan (kg)', validators=[DataRequired(message='data harus diisi')])
+    tb = IntegerField('Tinggi Badan (cm)', validators=[DataRequired(message='data harus diisi')])
     aktivitas = FloatField('Aktivitas', validators=[DataRequired(message='data harus diisi')])
     stress = FloatField('Faktor Stress', validators=[DataRequired(message='data harus diisi')])
-    gender = SelectField('Gender', choices=['PRIA', 'WANITA'])
-    hitung = SubmitField('Hitung', validators=[DataRequired(message='data harus diisi')])
-    laporan = SubmitField('Laporan', validators=[DataRequired(message='data harus diisi')])
-    
-    
+    gender = SelectField('Gender', choices=['pria', 'wanita'])
+    hitung = SubmitField('Hitung')
+    laporan = SubmitField('Laporan')
+    save = SubmitField('Save Data')
--- web/beranda/routes.py
+++ web/beranda/routes.py
@@ -1,6 +1,7 @@
-from flask import (render_template, redirect, Request, Blueprint, url_for)
+from flask import (render_template, redirect, request, Blueprint, url_for)
 from web.beranda.form import (FormDubois, FormHarrisBenneedict, FormMifflin)
- 
+from web.beranda.rumus.harris import Harris
+# from web.beranda.rumus.harris import harrisbenedict
 
 
 beranda = Blueprint('beranda', __name__)
@@ -27,13 +28,172 @@ def dubois_page():
     return render_template('dubois.html', form=form, tittle='DUBOIS')
 
 
-@beranda.route('/harris-bennedict')
+@beranda.route('/harris-bennedict', methods=['POST', 'GET'])
 def harris_bennedict_page():
-    form = FormHarrisBenneedict() 
-    return render_template('harris.html', form=form, tittle='HARRIS BENNEDICT')
+    form = FormHarrisBenneedict()
+    bb =''
+    tb = ''
+    umur =''
+    gender = ''
+    
+    imt = ''
+    bbi = ''
+    bee = ''
+    tee = ''
+    protein = ''
+    lemak = ''
+    karbo = ''
+    
+    energi_pagi = ''
+    protein_pagi = ''
+    lemak_pagi = ''
+    karbo_pagi = ''
+    
+    energi_siang = ''
+    protein_siang = ''
+    lemak_siang = ''
+    karbo_siang = ''
+    
+    energi_malam = ''
+    protein_malam = ''
+    lemak_malam = ''
+    karbo_malam = ''
+    
+    if request.method == 'POST' and 'bb' in request.form and 'tb' in request.form and 'umur' in request.form and 'gender' in request.form and 'stress' in request.form and 'aktivitas' in request.form:
+        bb = float(request.form.get('bb'))
+        tb = float(request.form.get('tb'))
+        umur = int(request.form.get('umur'))
+        gender = request.form.get('gender')
+        stress = float(request.form.get('stress'))
+        aktivitas = float(request.form.get('aktivitas'))
+        
+    if request.form.get('hitung'):
+        bb = float(request.form.get('bb'))
+        tb = float(request.form.get('tb'))
+        umur = int(request.form.get('umur'))
+        gender = request.form.get('gender')
+        
+        imt = round ((bb /(tb/100)**2),2)
+        bbi = round (0.9 * (tb-100),2)
+        
+        if request.form.get('gender') == 'pria':
+            bee = 66 + (13.7 * bb) + (5 * tb) - (6.8 * umur)
+        else:
+            bee = 655 + (9.6 * bb) + (1.8 * tb) - (4.7 * umur)
+
+        bee= round(bee,2)
+        tee = round((bee * aktivitas * stress),2 )
+        protein = round((0.15 * tee)/4, 2)
+        lemak = round((0.25 * tee)/9, 2)   
+        karbo  = round((0.65 * tee)/4, 2) 
+        
+        energi_pagi = round((0.35 * tee),2)
+        protein_pagi = round((0.35 * protein),2)
+        lemak_pagi = round((0.35 * lemak),2)
+        karbo_pagi = round((0.35 * karbo),2)
+        
+        energi_siang = round((0.35 * tee),2)
+        protein_siang = round((0.35 * protein),2)
+        lemak_siang = round((0.35 * lemak),2)
+        karbo_siang = round((0.35 * karbo),2)
+        
+        energi_malam = round((0.30 * tee),2)
+        protein_malam = round((0.30 * protein),2)
+        lemak_malam = round((0.30 * lemak),2)
+        karbo_malam = round((0.30 * karbo),2)
+        
+    return render_template('harris.html',tittle='HARRIS BENNEDICT', 
+                           form=form, bb=bb, tb=tb, umur=umur, 
+                           gender=gender,imt=imt, bbi=bbi, bee=bee, tee=tee, 
+                           protein=protein, lemak=lemak, karbo=karbo,
+                           energi_pagi=energi_pagi, protein_pagi=protein_pagi,
+                           lemak_pagi=lemak_pagi, karbo_pagi=karbo_pagi,
+                           energi_siang=energi_siang, protein_siang=protein_siang,
+                           lemak_siang=lemak_siang, karbo_siang=karbo_siang,
+                           energi_malam=energi_malam, protein_malam=protein_malam,
+                           lemak_malam=lemak_malam, karbo_malam=karbo_malam)
 
 
-@beranda.route('/mifflin')
+@beranda.route('/mifflin', methods=['POST', 'GET'])
 def mifflin_page():
     form = FormMifflin()
-    return render_template('mifflin.html', form=form, tittle='MIFFLIN')
+    bb =''
+    tb = ''
+    umur =''
+    gender = ''
+    
+    imt = ''
+    bbi = ''
+    bee = ''
+    tee = ''
+    protein = ''
+    lemak = ''
+    karbo = ''
+    
+    energi_pagi = ''
+    protein_pagi = ''
+    lemak_pagi = ''
+    karbo_pagi = ''
+    
+    energi_siang = ''
+    protein_siang = ''
+    lemak_siang = ''
+    karbo_siang = ''
+    
+    energi_malam = ''
+    protein_malam = ''
+    lemak_malam = ''
+    karbo_malam = ''
+    if request.method == 'POST' and 'bb' in request.form and 'tb' in request.form and 'umur' in request.form and 'gender' in request.form and 'stress' in request.form and 'aktivitas' in request.form:
+        bb = float(request.form.get('bb'))
+        tb = float(request.form.get('tb'))
+        umur = int(request.form.get('umur'))
+        gender = request.form.get('gender')
+        stress = float(request.form.get('stress'))
+        aktivitas = float(request.form.get('aktivitas'))
+        
+    if request.form.get('hitung'):
+        bb = float(request.form.get('bb'))
+        tb = float(request.form.get('tb'))
+        umur = int(request.form.get('umur'))
+        gender = request.form.get('gender')
+        
+        imt = round ((bb /(tb/100)**2),2)
+        bbi = round (0.9 * (tb-100),2)
+        
+        if request.form.get('gender') == 'pria':
+            bee = (10 * bb) + (6.25 * tb) - (5 * umur) + 5
+        else:
+            bee = (10 * bb) + (6.25 * tb) - (5 * umur) - 161
+
+        bee= round(bee,2)
+        tee = round((bee * aktivitas * stress),2 )
+        protein = round((0.15 * tee)/4, 2)
+        lemak = round((0.25 * tee)/9, 2)   
+        karbo  = round((0.65 * tee)/4, 2) 
+        
+        energi_pagi = round((0.35 * tee),2)
+        protein_pagi = round((0.35 * protein),2)
+        lemak_pagi = round((0.35 * lemak),2)
+        karbo_pagi = round((0.35 * karbo),2)
+        
+        energi_siang = round((0.35 * tee),2)
+        protein_siang = round((0.35 * protein),2)
+        lemak_siang = round((0.35 * lemak),2)
+        karbo_siang = round((0.35 * karbo),2)
+        
+        energi_malam = round((0.30 * tee),2)
+        protein_malam = round((0.30 * protein),2)
+        lemak_malam = round((0.30 * lemak),2)
+        karbo_malam = round((0.30 * karbo),2)
+        
+    return render_template('mifflin.html',tittle='MIFFLIN', 
+                           form=form, bb=bb, tb=tb, umur=umur, 
+                           gender=gender,imt=imt, bbi=bbi, bee=bee, tee=tee, 
+                           protein=protein, lemak=lemak, karbo=karbo,
+                           energi_pagi=energi_pagi, protein_pagi=protein_pagi,
+                           lemak_pagi=lemak_pagi, karbo_pagi=karbo_pagi,
+                           energi_siang=energi_siang, protein_siang=protein_siang,
+                           lemak_siang=lemak_siang, karbo_siang=karbo_siang,
+                           energi_malam=energi_malam, protein_malam=protein_malam,
+                           lemak_malam=lemak_malam, karbo_malam=karbo_malam)
