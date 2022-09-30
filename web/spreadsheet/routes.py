90cc76e28083c7191780cab6efbfc15de3a1104c
golDroger999 <goldr1711@gmail.com>
2022-09-30 22:37:56 +0800

    gizi kelompok hampir fix

--- web/spreadsheet/routes.py
+++ web/spreadsheet/routes.py
@@ -1,5 +1,6 @@
 from flask import (render_template, redirect, request, Blueprint, url_for, make_response)
 from web.spreadsheet.form import csv_form
+from web.spreadsheet.rumusgizi import HitungGiziKelompok as hgk 
 import pandas as pd
 import numpy as np
 
@@ -13,10 +14,29 @@ sheet = Blueprint('sheet', __name__)
 def csv_page():
     form = csv_form()
     data = ''
+    status = ''
+    energi = ''
+    protein = ''
+    lemak = ''
+    karbo = ''
+    
+    
     if request.method == 'POST':
-        data = pd.read_csv(request.files.get('csv'))
-        data = pd.DataFrame(data)
-        print(data)
+        
+        try:
+            data = pd.read_csv(request.files.get('csv'))
+            data = pd.DataFrame(data)
+            data['imt'] = (hgk(bb=data['bb'], tb=data['tb'], umur=data['umur'], gender=data['gender']).imt())
+            data['status'] = (hgk(bb=data['bb'], tb=data['tb'], umur=data['umur'], gender=data['gender']).statusgizi())
+            data['energi'] = (hgk(bb=data['bb'], tb=data['tb'], umur=data['umur'], gender=data['gender']).energi())
+            data['protein'] = (hgk(bb=data['bb'], tb=data['tb'], umur=data['umur'], gender=data['gender']).protein())
+            data['lemak'] = (hgk(bb=data['bb'], tb=data['tb'], umur=data['umur'], gender=data['gender']).lemak())
+            data['karbo'] = (hgk(bb=data['bb'], tb=data['tb'], umur=data['umur'], gender=data['gender']).karbo())
+            print(data)
+        
+        except:
+            data = 'maaf data tidak sesuai format'
+        
     return render_template('spreadsheet.html',title='GIZI KELOMPOK',form=form, data=data)
 #  AKHIR HALAMAN DASHBOARD CSV
 
--- web/spreadsheet/rumusgizi.py
+++ web/spreadsheet/rumusgizi.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 class HitungGiziKelompok():
     def __init__(self, bb, tb, umur, gender):
         self.bb = bb
@@ -5,6 +8,124 @@ class HitungGiziKelompok():
         self.umur = umur 
         self.gender = gender
         
+        
     def imt(self):
-        imt = self.bb / (self.tb*100)**2
-        return round(imt,2)
+        imt = self.bb / (self.tb/100)**2
+        return imt
+    
+    
+    
+    
+    def statusgizi(self):
+        imt = self.bb / (self.tb/100)**2
+        
+        data = [
+                (imt <  18.5),
+                (imt > 18.5 ) & (imt < 25),
+                (imt > 25 ) & (imt < 30),
+                (imt > 29 ) 
+                ]
+        
+        value = ['kurus', 'ideal', 'gemuk', 'obesitas']
+        
+        status = np.select(data, value)
+        return status
+    
+    
+    
+    
+    def energi(self, tidur=8, aktivitas=0.50):
+        bbi = 0.9 * (self.tb-100)
+        bmr_pria    = 1 * 24 * bbi
+        bmr_wanita  = 0.90 * 24 * bbi
+        
+        bmr_coice   = [
+            (self.gender == 'pria'),
+            (self.gender == 'wanita')
+            ]
+        
+        bmr = [bmr_pria, bmr_wanita]
+        bmr = np.select(bmr_coice, bmr)
+        
+        koreksi_tidur   = 0.1 * tidur * bbi
+        c_kalori        = bmr - koreksi_tidur
+        aktivitas       = aktivitas * c_kalori
+        e_kalori        = c_kalori + aktivitas
+        sda             = 0.1 * e_kalori
+        energi          = e_kalori + sda 
+        return (energi)
+    
+    
+    
+    
+    def protein(self, tidur=8, aktivitas=0.50):
+        bbi = 0.9 * (self.tb-100)
+        bmr_pria    = 1 * 24 * bbi
+        bmr_wanita  = 0.90 * 24 * bbi
+        
+        bmr_coice   = [
+            (self.gender == 'pria'),
+            (self.gender == 'wanita')
+            ]
+        
+        bmr = [bmr_pria, bmr_wanita]
+        bmr = np.select(bmr_coice, bmr)
+        
+        koreksi_tidur   = 0.1 * tidur * bbi
+        c_kalori        = bmr - koreksi_tidur
+        aktivitas       = aktivitas * c_kalori
+        e_kalori        = c_kalori + aktivitas
+        sda             = 0.1 * e_kalori
+        energi          = e_kalori + sda 
+        protein         = (0.15 * energi)/4
+        return (protein)
+    
+    
+    
+    
+    def lemak(self, tidur=8, aktivitas=0.50):
+        bbi = 0.9 * (self.tb-100)
+        bmr_pria    = 1 * 24 * bbi
+        bmr_wanita  = 0.90 * 24 * bbi
+        
+        bmr_coice   = [
+            (self.gender == 'pria'),
+            (self.gender == 'wanita')
+            ]
+        
+        bmr = [bmr_pria, bmr_wanita]
+        bmr = np.select(bmr_coice, bmr)
+        
+        koreksi_tidur   = 0.1 * tidur * bbi
+        c_kalori        = bmr - koreksi_tidur
+        aktivitas       = aktivitas * c_kalori
+        e_kalori        = c_kalori + aktivitas
+        sda             = 0.1 * e_kalori
+        energi          = e_kalori + sda 
+        lemak           = (0.25 * energi)/9
+        return (lemak)
+    
+    
+    
+    
+    def karbo(self, tidur=8, aktivitas=0.50):
+        bbi = 0.9 * (self.tb-100)
+        bmr_pria    = 1 * 24 * bbi
+        bmr_wanita  = 0.90 * 24 * bbi
+        
+        bmr_coice   = [
+            (self.gender == 'pria'),
+            (self.gender == 'wanita')
+            ]
+        
+        bmr = [bmr_pria, bmr_wanita]
+        bmr = np.select(bmr_coice, bmr)
+        
+        koreksi_tidur   = 0.1 * tidur * bbi
+        c_kalori        = bmr - koreksi_tidur
+        aktivitas       = aktivitas * c_kalori
+        e_kalori        = c_kalori + aktivitas
+        sda             = 0.1 * e_kalori
+        energi          = e_kalori + sda 
+        karbo = (0.65 * energi)/4 
+        return (karbo)
